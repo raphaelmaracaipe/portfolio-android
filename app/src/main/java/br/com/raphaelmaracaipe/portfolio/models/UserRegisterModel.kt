d4b14e24df125f88ed7b928cbef317158b56c576
Raphael S Maracaipe <raphaelmaracaipe@gmail.com>
2022-11-01 21:09:54 -0300

    Inclusão de parse.

--- app/src/main/java/br/com/raphaelmaracaipe/portfolio/models/UserRegisterModel.kt
+++ app/src/main/java/br/com/raphaelmaracaipe/portfolio/models/UserRegisterModel.kt
@@ -1,5 +1,13 @@
 package br.com.raphaelmaracaipe.portfolio.models
 
+import android.os.Parcelable
+import androidx.annotation.Keep
+import kotlinx.android.parcel.Parcelize
+
+@Keep
+@Parcelize
 data class UserRegisterModel(
-    val email: String = "", var password: String = "", var code: String = ""
-)
+    val email: String = "",
+    var password: String = "",
+    var code: String = ""
+): Parcelable
