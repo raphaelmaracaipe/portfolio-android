fa638db723edfa4e9b6b906100a3641301349017
Raphael S Maracaipe <raphaelmaracaipe@gmail.com>
2022-11-18 16:09:13 -0300

    Criação de funcionalidade para atualização da senha.

--- app/src/main/java/br/com/raphaelmaracaipe/portfolio/ui/main/MainActivity.kt
+++ app/src/main/java/br/com/raphaelmaracaipe/portfolio/ui/main/MainActivity.kt
@@ -2,7 +2,6 @@ package br.com.raphaelmaracaipe.portfolio.ui.main
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -10,8 +9,10 @@ import br.com.raphaelmaracaipe.portfolio.App
 import br.com.raphaelmaracaipe.portfolio.R
 import br.com.raphaelmaracaipe.portfolio.const.EVENT_KEY_LOADING
 import br.com.raphaelmaracaipe.portfolio.databinding.ActivityMainBinding
+import br.com.raphaelmaracaipe.portfolio.ui.userUpdatePassword.UpdatePasswordBottomSheet
 import br.com.raphaelmaracaipe.portfolio.utils.events.Event
 import br.com.raphaelmaracaipe.portfolio.utils.events.EventModule
+import com.google.android.material.snackbar.Snackbar
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -29,6 +30,30 @@ class MainActivity : AppCompatActivity() {
         settingActionBar()
         callbackOfEventLoading()
         event.registerEvent(EVENT_KEY_LOADING)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        getInformationDeepLink()
+    }
+
+    private fun getInformationDeepLink() {
+        intent.data.apply {
+            val urlSplitted = toString().split("=")
+            if(urlSplitted.size == 2) {
+                val code = urlSplitted[1]
+                showBottomSheetToUpdatePassword(code)
+            }
+        }
+    }
+
+    private fun showBottomSheetToUpdatePassword(code: String) {
+        UpdatePasswordBottomSheet.showAlertToUpdatePassword(
+            supportFragmentManager,
+            code
+        ) { msgRes ->
+            Snackbar.make(bind.root, msgRes, Snackbar.LENGTH_LONG).show()
+        }
     }
 
     private fun settingActionBar() {
---
+++ app/src/main/java/br/com/raphaelmaracaipe/portfolio/ui/userUpdatePassword/UpdatePasswordViewModel.kt
@@ -0,0 +1,9 @@
+package br.com.raphaelmaracaipe.portfolio.ui.userUpdatePassword
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class UpdatePasswordViewModel @Inject constructor(
+
+): ViewModel() {
+}
