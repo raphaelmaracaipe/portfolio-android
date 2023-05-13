49fd8276c0e134a5325620da8b4305ac9605e82a
Raphael S Maracaipe <raphaelmaracaipe@gmail.com>
2023-05-13 09:26:31 -0300

    Correção de erro na importação.

--- app/src/main/java/br/com/raphaelmaracaipe/portflio/MainApplication.kt
+++ app/src/main/java/br/com/raphaelmaracaipe/portflio/MainApplication.kt
@@ -6,6 +6,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.startKoin
+import org.koin.core.logger.EmptyLogger
 
 class MainApplication : Application() {
 
