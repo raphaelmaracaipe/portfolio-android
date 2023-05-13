295912078b4106c39455f473daf75680c7a659f7
Raphael S Maracaipe <raphaelmaracaipe@gmail.com>
2023-05-13 13:20:11 -0300

    Remoção de código não usado.

--- uiauth/src/main/java/br/com/raphaelmaracaipe/uiauth/data/PhoneRepository.kt
+++
@@ -1,7 +0,0 @@
-package br.com.raphaelmaracaipe.data
-
-import br.com.raphaelmaracaipe.uiauth.models.CodeCountry
-
-interface PhoneRepository {
-    suspend fun getCodeOfCountry(): Array<CodeCountry>?
-}
